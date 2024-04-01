29fd2690699b49682b6543709c7e5afbdb0b56cf
manikandanc01 <cmanikandan680@gmail.com>
2024-04-01 19:23:49 +0530

    userservice with random string token

--- userservice/userservice/src/main/java/com/example/userservice/UserserviceApplication.java
+++ userservice/userservice/src/main/java/com/example/userservice/UserserviceApplication.java
@@ -1,5 +1,6 @@
 package com.example.userservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(UserserviceApplication.class, args);
 	}
 
